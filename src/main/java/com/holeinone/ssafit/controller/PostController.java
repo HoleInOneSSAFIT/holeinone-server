29eab8c0cae86764b9a0778c7b5a775439281e94
yoonjeongKm <yoommnj@gmail.com>
2025-05-21 10:04:14 +0900

    git commit -m "fix: 게시글 기본 구조"

--- src/main/java/com/holeinone/ssafit/controller/PostController.java
+++ src/main/java/com/holeinone/ssafit/controller/PostController.java
@@ -1,4 +1,19 @@
 package com.holeinone.ssafit.controller;
 
+import com.holeinone.ssafit.model.service.PostService;
+import com.holeinone.ssafit.model.service.VideoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/post")
 public class PostController {
+
+    private final PostService postService;
+
+
 }
--- src/main/java/com/holeinone/ssafit/model/service/PostServiceImpl.java
+++ src/main/java/com/holeinone/ssafit/model/service/PostServiceImpl.java
@@ -1,4 +1,9 @@
 package com.holeinone.ssafit.model.service;
 
-public class PostServiceImpl {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PostServiceImpl implements PostService{
 }
